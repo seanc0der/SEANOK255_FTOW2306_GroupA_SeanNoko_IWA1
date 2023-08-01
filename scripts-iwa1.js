a7bce1fbe1b33cd964bb206f97d05e6b69a62098
seanc0der <sean.noko@gmail.com>
2023-08-01 21:01:57 +0200

    added step_amount const and updated the subtractHandler and addHandler functions accordingly with the const.

--- scripts-iwa1.js
+++ scripts-iwa1.js
@@ -1,12 +1,13 @@
-const MIN_NUMBER = -5;
-const MAX_NUMBER = 10;
+const MIN_NUMBER = -10;
+const MAX_NUMBER = 15;
+const STEP_AMOUNT = 5;
 
 const number = document.querySelector('[data-key="number"]');
 const subtract = document.querySelector('[data-key="subtract"]');
 const add = document.querySelector('[data-key="add"]');
 
 const subtractHandler = () => {
-	const newValue = parseInt(number.value) - 1;
+	const newValue = parseInt(number.value) - STEP_AMOUNT;
 	number.value = newValue;
 
 	if (newValue <= MIN_NUMBER) {
@@ -19,7 +20,7 @@ const subtractHandler = () => {
 };
 
 const addHandler = () => {
-	const newValue = parseInt(number.value) + 1;
+	const newValue = parseInt(number.value) + STEP_AMOUNT;
 	number.value = newValue;
 
 	if (newValue >= MAX_NUMBER) {
